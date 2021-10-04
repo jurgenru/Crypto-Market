913619445772ba70d8b098657f36c58e076c086b
benji.8500 <benji.8500@gmail.com>
2021-10-04 09:44:51 -0400

    missing files

---
+++ app/src/main/java/com/example/cryptomarket/model/Coin.kt
@@ -0,0 +1,6 @@
+package com.example.cryptomarket.model
+
+data class Coin(val coinName: String,
+                val shortDescription: String,
+                val imageUrl: String,
+                val price: Double)
