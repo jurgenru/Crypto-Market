4c1f3dc502a7796f764cda4be41486c3f5d726a7
jurgenru <jurgenruegnberg@gmail.com>
2021-10-25 20:22:19 -0400

    Login Nav graph and view

---
+++ app/src/main/java/com/example/cryptomarket/ui/login/fragments/LogInFragment.kt
@@ -0,0 +1,29 @@
+package com.example.cryptomarket.ui.login.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.cryptomarket.databinding.FragmentLoginBinding
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+
+class LogInFragment: Fragment() {
+    private lateinit var binding: FragmentLoginBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.btSignUpEmail.setOnClickListener {
+            val directions = LogInFragmentDirections.actionLogInFragmentToMainMenuNavGraph2()
+            findNavController().navigate(directions)
+        }
+    }
+}
